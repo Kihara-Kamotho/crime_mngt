b31b90dfbf736109932926a6defe02ddfa1ba5e1
Kihara-Kamotho <kiharaalphan@gmail.com>
2020-10-02 07:30:45 +0300

    changed port

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module CrimeMngt15
 
     ActionMailer::Base.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
+      port:                 456,
       domain:               'mail.google.com',
       user_name:            ENV['GMAIL_USERNAME'],
       password:             ENV['GMAIL_PASSWORD'], 
